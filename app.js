7ee3fc31d30d033b6d53f1a291207730d8fc8b31
jpwebdev <jjpwebdev@gmail.com>
2018-02-06 08:01:52 -0500

    All modules required in app.js, middleware setup included for Mongoose, Body-Parser, Express-Handlebars and Static Folder

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const exphbs  = require('express-handlebars');
+const request = require('request');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+// Middleware: Static Public Folder
+app.use(express.static('public'));
+
+// Mongoose connection
+mongoose.connect('mongodb://localhost/mongoosebasicsDB');
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Middleware: Body-Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Middleware: Express-Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
